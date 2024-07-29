321efaf6553c05eeccf0dda17e9c2c94c341093f
lyinggame4356 <158070999+lyinggame4356@users.noreply.github.com>
2024-07-29 15:05:57 +0300

    first file

---
+++ src/js/script.js
@@ -0,0 +1,13 @@
+/* leny/meet-gmap
+ *
+ * /src/js/script.js - Main script (jQuery Version)
+ *
+ * coded by leny@flatLand!
+ * started at 06/03/2017
+ */
+
+$( function() {
+
+    console.log( "Kikoo!" );
+
+} );
